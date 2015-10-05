28caf153bba397b9fd3a52e49bee826eb248d4d3
rsanjoseestepar <rsanjoseestepar@gmail.com>
2015-10-05 16:18:26 -0400

    worker deleting debugging 11

--- worker/views.py
+++ worker/views.py
@@ -23,8 +23,8 @@ def delete_model(id,user):
         a = ImgContainer.objects.get(pk=id)
         print a.user_id
         print user
-        print "a"+a.user_id+"a"
-        print "a"+user+"a"
+        print "b" + a.user_id + "b"
+        print "b" + user + "b"
         print type(a.user_id)
         print type(user)
         if (a.user_id == user):
