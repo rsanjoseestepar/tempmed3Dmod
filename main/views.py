4e097f7d55a1268a8254c12d152ef8b45aa670a6
rsanjoseestepar <rsanjoseestepar@gmail.com>
2015-10-05 17:44:45 -0400

    worker deleting debugging 21

--- main/views.py
+++ main/views.py
@@ -225,6 +225,7 @@ def generate(request,id):
 	#print html
 	if request.user.is_authenticated():
 		user=get_user(request)
+
 		values = {'id' : id, 'user' : user.id }
 		data = urllib.urlencode(values)
 		w = SendWorker()
--- worker/views.py
+++ worker/views.py
@@ -3,30 +3,75 @@ from django.http import *
 import time, urllib, urllib2
 from main.models import *
 from django.contrib.auth import *
+import os, subprocess
 # Create your views here.
-def worker(request):
+# def worker(request):
+#
+#     if request.method == 'GET':
+#         time.sleep(5)
+#         print "slept and deleted"
+#         id = request.GET.get('id')
+#         user = request.GET.get('user')
+#         print id + user
+#         delete_model(id,user)
+#         return HttpResponse("Done deleting")
+#     else:
+#         return HttpResponseForbidden
+#
+#
+# def delete_model(id,user):
+#
+#         a = ImgContainer.objects.get(pk=id)
+#         print a.user_id
+#         print user
+#         user = int(user)
+#         print type(a.user_id)
+#         print type(user)
+#         if (a.user_id == user):
+#             a.active = False
+#             a.save()
+#             return
 
+
+
+def worker(request):
     if request.method == 'GET':
-        time.sleep(5)
-        print "slept and deleted"
         id = request.GET.get('id')
         user = request.GET.get('user')
-        print id + user
-        delete_model(id,user)
-        return HttpResponse("Done deleting")
-    else:
-        return HttpResponseForbidden
-
-
-def delete_model(id,user):
-
         a = ImgContainer.objects.get(pk=id)
-        print a.user_id
-        print user
+        file = a.file
+        print file
         user = int(user)
-        print type(a.user_id)
-        print type(user)
         if (a.user_id == user):
-            a.active = False
-            a.save()
+
+            # subprocess.call(['./test.sh'])
             return
+
+# def script():
+#     caseLists = `ls -1 *BWH.nrrd | cut -d "_labelmap.nrrd" -f1`
+#
+#     shellThickness=20
+#
+#     for cc in $caseLists; do
+#
+#
+#     unu 2op gte ${cc}.nrrd 1 -o tmp.nrrd
+#     ComputeDistanceMap -l tmp.nrrd -m Maurer -d tmp-dm.nrrd -p
+#
+#     unu 3op in_cl 0 tmp-dm.nrrd $shellThickness -t ushort -o tmp-lm.nrrd
+#
+#     #GenerateModel -i tmp-lm.nrrd -l 1 -o ${cc}.vtk
+#     /Applications/Slicer.app/Contents/lib/Slicer-4.4/cli-modules/ModelMaker tmp-lm.nrrd -l 1 -n ${cc}
+#
+#     #Computing model from raw
+#     #GenerateModel -i tmp.nrrd -l 1 -o cip-raw.vtk
+#     #/Applications/Slicer.app/Contents/lib/Slicer-4.4/cli-modules/ModelMaker tmp.nrrd -l 1 -n slicer-raw
+#
+#     #Compute a downsampled model for quicker displayed
+#     unu resample -k cubic:1,0 -i tmp-dm.nrrd -s x0.5 x0.5 = -o tmp-dm-ds.nrrd
+#     unu 3op in_cl 0 tmp-dm-ds.nrrd $shellThickness -t ushort -o tmp-lm-ds.nrrd
+#
+#     #GenerateModel -i tmp-lm-ds.nrrd -l 1 -o cip-ds.vtk
+#     /Applications/Slicer.app/Contents/lib/Slicer-4.4/cli-modules/ModelMaker tmp-lm-ds.nrrd -l 1 -n ${cc}_downsampled
+#
+#     done
