c1a79d431b3e96d75455146cbfe06ea61d548df5
rsanjoseestepar <rsanjoseestepar@gmail.com>
2015-10-05 14:59:19 -0400

    worker borrador d

--- worker/views.py
+++ worker/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from django.http import *
 import time, urllib, urllib2
+from main.models import *
+from django.contrib.auth import *
 # Create your views here.
 def worker(request):
-    if request.method == 'GET':
-        time.sleep(5)
-        print "slept and deleted"
-        id = request.GET.get('id')
-        delete_model(id)
-        return HttpResponse("Done deleting")
-    else:
-        return HttpResponseForbidden
+    if request.user.is_authenticated():
+        user=get_user(request)
+        if request.method == 'GET':
+            time.sleep(5)
+            print "slept and deleted"
+            id = request.GET.get('id')
+            delete_model(id, user)
+            return HttpResponse("Done deleting")
+        else:
+            return HttpResponseForbidden
 
 
-def delete_model(id):
+def delete_model(id,user):
 
-		a=Model.objects.get(pk=id)
-		if (a.user_id == user.id):
-			a.active = False
-			a.save()
+        a = Model.objects.get(pk=id)
+        if (a.user_id == user.id):
+            a.active = False
+            a.save()
