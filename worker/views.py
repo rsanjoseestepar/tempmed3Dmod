d8fbd750c93b3746ab52edb685918452b6924c25
rsanjoseestepar <rsanjoseestepar@gmail.com>
2015-10-05 14:43:54 -0400

    worker borrador

--- worker/views.py
+++ worker/views.py
@@ -6,15 +6,16 @@ def worker(request):
     if request.method == 'GET':
         time.sleep(5)
         print "slept and deleted"
-        print request.GET.get('id')
-        return HttpResponse("Done")
+        id = request.GET.get('id')
+        delete_model(id)
+        return HttpResponse("Done deleting")
     else:
         return HttpResponseForbidden
 
 
-# def delete_model(id):
-#
-# 		a=Model.objects.get(pk=id)
-# 		if (a.user_id == user.id):
-# 			a.active = False
-# 			a.save()
+def delete_model(id):
+
+		a=Model.objects.get(pk=id)
+		if (a.user_id == user.id):
+			a.active = False
+			a.save()
